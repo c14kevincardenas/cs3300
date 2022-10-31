db15167a11051f38742a420f75c71580712f70f3
c14kevincardenas <kevincardenas4@gmail.com>
2022-10-31 16:22:42 +0000

    tests

--- spec/controller/projects_spec.rb
+++ spec/controller/projects_spec.rb
@@ -16,4 +16,23 @@ RSpec.describe ProjectsController, type: :controller do
       expect(response).to be_success
     end
   end
+
+  context "GET #new" do
+    let!(:project) { Project.create(title: "Test title", description: "Test description") }
+    it "returns a success response" do
+      count = Project.count
+      get :new
+      # expect(response.success).to eq(true)
+      expect(Project.count).to eq(count)
+    end
+  end
+
+  context "GET #edit" do
+    it "returns a success response" do
+      get :edit
+      # expect(response.success).to eq(true)
+      expect(response).to be_success
+    end
+  end
+
 end
